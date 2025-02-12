8ba242dc753d5e6a976834679abf9903d8d6674f
amjad-228 <amjadalsabry28@gmail.com>
2025-02-12 18:58:30 +0300

    seo new

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,22 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
+  site: "https://www.sathhat-jeddah.com",
+  integrations: [
+    sitemap({
+      customPages: [
+        {
+          url: "https://www.sathhat-jeddah.com/",
+          priority: 1.0,
+        },
+        {
+          url: "https://www.sathhat-jeddah.com/contact",
+          priority: 0.8,
+        }
+      ]
+    })],
   integrations: [tailwind()],
   site: 'https://sathhat-jeddah.com',
   i18n: {
